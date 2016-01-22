e1ab952a0d86ae230913be8b523a3b3a68e3a420
Luis <luis.f.blanco@gmail.com>
2016-01-21 20:11:03 -0600

    Added AngularJS

---
+++ public/application.js
@@ -0,0 +1,8 @@
+var mainAppModuleName = 'myApp';
+
+var mainApplicatonModule = angular.module(mainAppModuleName, ['example']);
+
+// angular.element(document).ready(function(){
+//     angular.bootstrap(document, [mainAppModuleName]);
+// });
+
